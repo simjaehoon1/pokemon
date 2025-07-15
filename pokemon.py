a8a40b12b3d2fd581b1d30c5b04a36cf5b33e02f
simjaehoon1 <sangdi1234@naver.com>
2025-07-15 15:13:41 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,7 @@
 # 3. 크롤링 - Selenium
 #  - 브라우저를 켜지 않고도 할 수 있는가?
 #  - linux 크롬 드라이버!! 
+
 import selenium.webdriver as wb
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,13 +12,16 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path="/usr/bin/chromedriver")
+
 options = Options()
 options.add_argument("--headless") # 창 없음
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 url = "https://pokemonkorea.co.kr/pokedex"
-driver = wb.Chrome()
+driver = wb.Chrome(service=service, options=options)
 driver.maximize_window()
 driver.get(url)
 img = driver.find_element(By.CSS_SELECTOR, "img.img-fluid")
@@ -32,9 +36,12 @@ import csv
 import os
 pokemon_exist = os.path.exists("pokemon.csv")
 header = ["no", "name"]
+
 with open("pokemon.csv", "a", newline="") as file:
     writer = csv.writer(file)
+
     if not pokemon_exist:
         writer.writerow(header)
+
     writer.writerow(["0001", name])
     print("포켓몬 저장 완료!!")
